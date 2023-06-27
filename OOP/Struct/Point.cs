b1a615e9817ca475c35e76266a0da1a83ca1fd96
Ajadne Vacineski <ajadne856@gmail.com>
2023-06-27 17:58:26 -0300

    Structs

---
+++ OOP/Struct/Point.cs
@@ -0,0 +1,14 @@
+using System.Drawing;
+namespace Course
+{
+    struct Point
+    {
+
+        public double X;
+        public double Y;
+
+        public override string ToString(){
+            return "(" + X + ", "+ Y + ")";
+        }
+    }
+}
---
+++ OOP/Struct/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point;
+            point.X = 10;
+            point.Y = 20;
+            System.Console.WriteLine(point);
+            point = new Point();
+            System.Console.WriteLine(point);
+
+            //nullable
+
+            double? x = null;
+            double? y = 0.5;
+
+            //Retrieves the value of the current Nullable<T> object, or a default value.
+            System.Console.WriteLine(x.GetValueOrDefault());
+            System.Console.WriteLine(y.GetValueOrDefault());
+
+            //Gets a value indicating whether the current Nullable<T> object has a valid value of its underlying type.
+            System.Console.WriteLine(x.HasValue);
+            System.Console.WriteLine(y.HasValue);
+
+            //Gets the value of the current Nullable<T> object if it has been assigned a valid underlying value.
+            System.Console.WriteLine(x.Value); //InvalidOperationException 
+            System.Console.WriteLine(y.Value);
+
+            if (x.HasValue)
+            {
+                System.Console.WriteLine(x.Value);
+            }
+            else
+            {
+                System.Console.WriteLine("X is null");
+            }
+
+            if (y.HasValue)
+            {
+                System.Console.WriteLine(y.Value);
+            }
+            else
+            {
+                System.Console.WriteLine("Y is null");
+            }
+
+        }
+    }
+}
