a79408d5152bd79341029097fb8340cee136e570
Ajadne Vacineski <ajadne856@gmail.com>
2023-06-28 16:39:45 -0300

    first lesson on vectors

---
+++ OOP/Vectors/Program.cs
@@ -0,0 +1,33 @@
+﻿
+//program to calculate the average of a given number of heights
+
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine());
+            }
+
+            double sum = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += vect[i];
+            }
+
+            double avg = sum / n;
+
+            Console.WriteLine("Average height: " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
