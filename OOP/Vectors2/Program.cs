ce1bd93f7b889db5eb4b4a35ff2b4cbae9328fb4
Ajadne Vacineski <ajadne856@gmail.com>
2023-06-28 17:17:43 -0300

    second example about vectors

---
+++ OOP/Vectors2/Program.cs
@@ -0,0 +1,36 @@
+﻿//program that calculates the average price of a quantity of products
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.WriteLine("Enter the quantity of products: ");
+            int n = int.Parse(Console.ReadLine());
+            Product[] vect = new Product[n];
+        
+
+            for (int i = 0; i < n; i++)
+            {
+                System.Console.WriteLine("Enter product name: ");
+                string name = Console.ReadLine();
+                System.Console.WriteLine("Enter product price: ");
+                double price = double.Parse(Console.ReadLine());
+                vect[i] = new Product { Name = name, Price = price };
+            }
+
+            double sum = 0.0;
+
+            for (int i = 0; i < n; i++){
+                sum += vect[i].Price;
+            }
+
+            double avg = sum / n;
+
+            Console.WriteLine("Average Price: $" + avg.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
