ae7e0df785a57f2cf3141fe80a51b7bf6143a6da
Ajadne Vacineski <ajadne856@gmail.com>
2023-06-29 13:52:26 -0300

    access modifiers

---
+++ OOP/Params/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Course{
+    class Program{
+        public static void Main(string[] args){
+            int s1 = Calculator.Sum(new int[]{2,4});
+            int s2 = Calculator.Sum(new int[] {2,4,6});
+
+        }
+    }
+}
