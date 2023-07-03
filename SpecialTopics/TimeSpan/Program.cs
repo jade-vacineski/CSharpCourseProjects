e37469e5c8acc771e5cdf56a37ba12416e5972c9
Ajadne Vacineski <ajadne856@gmail.com>
2023-07-03 16:51:06 -0300

    representing durations

---
+++ SpecialTopics/TimeSpan/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TimeSpan t1 = new TimeSpan(0, 1, 30);
+            Console.WriteLine(t1);
+            Console.WriteLine(t1.Ticks);
+
+            TimeSpan t2 = new TimeSpan(1, 2, 11, 21);
+            Console.WriteLine(t2);
+
+            TimeSpan t3 = TimeSpan.FromDays(1.5);
+            Console.WriteLine(t3);
+
+            TimeSpan t4 = TimeSpan.FromHours(1.5);
+            Console.WriteLine(t4);
+
+            TimeSpan t5 = TimeSpan.FromMinutes(1.5);
+            Console.WriteLine(t5);
+
+            TimeSpan t6 = TimeSpan.FromSeconds(1.5);
+            Console.WriteLine(t6);
+
+        }
+    }
+}
