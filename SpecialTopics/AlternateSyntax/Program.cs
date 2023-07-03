3aa6d143da5c602e8e76305a6f0d76dbd9ef0c0d
Ajadne Vacineski <ajadne856@gmail.com>
2023-07-03 13:36:15 -0300

    ternary conditional expression

---
+++ SpecialTopics/AlternateSyntax/Program.cs
@@ -0,0 +1,15 @@
+﻿//ternary conditional expression
+
+namespace course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double price = double.Parse(Console.ReadLine());
+            double discount = (price < 20.0) ? price * 0.1 : price * 0.5;
+            System.Console.WriteLine(discount);
+
+        }
+    }
+}
