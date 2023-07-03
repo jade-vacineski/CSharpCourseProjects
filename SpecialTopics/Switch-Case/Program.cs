b37567202953f14c6278157984ae79883daf3ea6
Ajadne Vacineski <ajadne856@gmail.com>
2023-07-03 13:18:33 -0300

    switch case

---
+++ SpecialTopics/Switch-Case/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Couse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int x = int.Parse(Console.ReadLine());
+            string day;
+
+            switch (x)
+            {
+                case 1:
+                    day = "sunday";
+                    break;
+                case 2:
+                    day = "Monday";
+                    break;
+                case 3:
+                    day = "Tuesday";
+                    break;
+                case 4:
+                    day = "Wednesday";
+                    break;
+                case 5:
+                    day = "Thursday";
+                    break;
+                case 6:
+                    day = "Friday";
+                    break;
+                case 7:
+                    day = "Saturday";
+                    break;
+                default:
+                    day = "Invalid value";
+                    break;
+
+            }
+
+            System.Console.WriteLine("Day: " + day);
+
+        }
+    }
+}
