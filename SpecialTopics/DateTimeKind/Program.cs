3878a61321e479f6e56acf4f226f953c77a2daf6
Ajadne Vacineski <ajadne856@gmail.com>
2023-07-03 17:27:25 -0300

    DateTimeKind

---
+++ SpecialTopics/DateTimeKind/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+namespace course
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            DateTime d1 = new DateTime(2023, 7, 3, 5, 58, (int)DateTimeKind.Local);
+            DateTime d2 = new DateTime(2023, 7, 3, 5, 58, (int)DateTimeKind.Utc);
+            System.Console.WriteLine(d1.Kind);
+            System.Console.WriteLine(d1.ToLocalTime());
+            System.Console.WriteLine(d1.ToUniversalTime());
+            
+
+            
+        }
+    }
+}
