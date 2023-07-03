085c7589061af352fe386bae666b8439abe1a0f2
Ajadne Vacineski <ajadne856@gmail.com>
2023-07-03 12:57:07 -0300

    special topics in c#

---
+++ SpecialTopics/KeywordVar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace course;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        var y = 10.5;
+        var x = 10;
+        var name = "maria";
+
+        System.Console.WriteLine(x);
+        System.Console.WriteLine(y);
+        System.Console.WriteLine(name);
+
+        
+    }
+}
+   
