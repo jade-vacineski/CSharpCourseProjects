2a8c5eaecbc31a95f8333813d1a894ddf5759cf6
Ajadne Vacineski <ajadne856@gmail.com>
2023-07-03 11:23:02 -0300

    Matrices

---
+++ OOP/Matrices/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of the matrix: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            System.Console.WriteLine("enter the matrix values");
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            System.Console.WriteLine("Main diagonal: ");
+            for (int i = 0; i < n; i++)
+            {
+                System.Console.WriteLine(mat[i, i] + " ");
+            }
+
+            System.Console.WriteLine();
+
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Negative number: " + count);
+
+        }
+    }
+}
