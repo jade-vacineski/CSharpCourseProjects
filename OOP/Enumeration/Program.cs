1a08b610aa00959b8f539d8c093fc5f7119c9b10
Ajadne Vacineski <ajadne856@gmail.com>
2023-07-04 11:11:09 -0300

    Enumeration types

---
+++ OOP/Enumeration/Program.cs
@@ -0,0 +1,28 @@
+﻿using course.Entities;
+using course.Entities.Enums;
+
+namespace course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Order order = new Order()
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment,
+            };
+
+            System.Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            System.Console.WriteLine(txt);
+
+            OrderStatus orderStatus = Enum.Parse<OrderStatus>("Delivered");
+            System.Console.WriteLine(orderStatus);
+
+        }
+    }
+}
