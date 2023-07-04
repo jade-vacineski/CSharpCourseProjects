fe4ab7ace3bc7119a46e095e957068aff522f7a6
Ajadne Vacineski <ajadne856@gmail.com>
2023-07-04 10:51:40 -0300

    Enumeration

---
+++ OOP/Enumeration/Entities/Order.cs
@@ -0,0 +1,22 @@
+using course.Entities.Enums;
+
+namespace course.Entities
+{
+    class Order
+    {
+
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id
+            + ", "
+            + Moment
+            + ", "
+            + Status;
+        }
+
+    }
+}
