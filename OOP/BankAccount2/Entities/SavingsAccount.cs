0077826da8c67509b9b0fb87661f4e98fafcc510
Ajadne Vacineski <ajadne856@gmail.com>
2023-07-05 17:02:42 -0300

    Inheritance

---
+++ OOP/BankAccount2/Entities/SavingsAccount.cs
@@ -0,0 +1,16 @@
+namespace course.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+
+        }
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+    }
+}
---
+++ OOP/BankAccount2/Program.cs
@@ -0,0 +1,14 @@
+﻿using course.Entities;
+
+namespace course
+{
+
+    class Program
+    {
+        static void Main(string[] args){
+
+            BusinessAccount account= new BusinessAccount(8010, "Bob Bob", 100.0, 500.0);
+            System.Console.WriteLine(account.Balance);
+        }
+    }
+}
