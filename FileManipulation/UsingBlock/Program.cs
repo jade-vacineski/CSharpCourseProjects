2068b157725f2ee84c55edfa025226449a30c16f
Ajadne Vacineski <ajadne856@gmail.com>
2023-07-18 17:08:10 -0300

    Using Block

---
+++ FileManipulation/UsingBlock/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Course\FileManipulation\file1.txt";
+
+            try
+            {
+                using StreamReader sr = File.OpenText(path);
+
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
