a65723ce91210302c9a2e2091b3d336ff606ea34
Ajadne Vacineski <ajadne856@gmail.com>
2023-07-18 16:50:39 -0300

    FileStream and StreamReader

---
+++ FileManipulation/FileStream/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Course\FileManipulation\file1.txt";
+
+            StreamReader streamReader = null;
+            try
+            {
+                streamReader = File.OpenText(path);
+                while (!streamReader.EndOfStream)
+                {
+                    string line = streamReader.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+            finally
+            {
+                streamReader?.Close();
+
+            }
+        }
+    }
+}
