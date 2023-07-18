83e85754bb91d73f5a3ed309c94e07374d97d77b
Ajadne Vacineski <ajadne856@gmail.com>
2023-07-18 16:13:12 -0300

    File, FileInfo and IOException

---
+++ FileManipulation/Reading/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Course\FileManipulation\file1.txt";
+            string targetPath = @"C:\Course\FileManipulation\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    System.Console.WriteLine(line);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
