0b361bc70d36fac1818515f9ba98e300e48cce4d
Ajadne Vacineski <ajadne856@gmail.com>
2023-07-19 14:20:47 -0300

    Directory, DirectoryInfo

---
+++ FileManipulation/Directory/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Course\FileManipulation\Directory\myfolder";
+
+            try
+            {
+                var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                System.Console.WriteLine("Folders: ");
+                foreach (string s in folders)
+                {
+                    System.Console.WriteLine(s);
+                }
+
+                
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                System.Console.WriteLine("Files: ");
+                foreach (string s in files)
+                {
+                    System.Console.WriteLine(s);
+                }
+
+                Directory.CreateDirectory(path + "\\new folder");
+
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
