9f38c6fb621f7c0e592377edd243f2de2609c08b
Ajadne Vacineski <ajadne856@gmail.com>
2023-07-19 10:23:44 -0300

    StreamWriter

---
+++ FileManipulation/StreamWriter/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"C:\Course\FileManipulation\file1.txt";
+            string targetPath = @"C:\Course\FileManipulation\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(path);
+
+                using StreamWriter sw = File.AppendText(targetPath);
+
+                foreach (string line in lines)
+                {
+                    sw.WriteLine(line.ToUpper());
+                }
+
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
