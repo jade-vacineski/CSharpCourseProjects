04ac38af802f55d9c042bca4014bd407795ecf98
Ajadne Vacineski <ajadne856@gmail.com>
2023-07-21 10:23:59 -0300

    exercise solution without interface

---
+++ Interface/CarRent/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using CarRent.Services;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.WriteLine("Enter rental data: ");
+            System.Console.WriteLine("Car model: ");
+            string model = Console.ReadLine();
+            System.Console.WriteLine("Pickup (dd/MM/yyyy hh:ss): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            System.Console.WriteLine("Return (dd/MM/yyyy hh:ss): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            System.Console.WriteLine("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            System.Console.WriteLine("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            var carRental = new CarRental(start, finish, new Vehicle(model));
+            var rentalService = new RentalService(hour, day);
+
+            rentalService.ProcessInvoice(carRental);
+            System.Console.WriteLine("Invoice");
+            System.Console.WriteLine(carRental.Invoice);
+
+        }
+    }
+}
---
+++ Interface/CarRent/Services/BrazilTaxService.cs
@@ -0,0 +1,19 @@
+namespace CarRent.Services
+{
+    public class BrazilTaxService
+    {
+
+        public double Tax(double amount)
+        {
+            if (amount <= 100.0)
+            {
+                return amount * 0.2;
+            }
+            else
+            {
+                return amount * 0.15;   
+            }
+        }
+        
+    }
+}
