e9ae82de7d1c79bc9fed22aeb6c080ebfda8d168
Ajadne Vacineski <ajadne856@gmail.com>
2023-07-26 13:15:47 -0300

    exercise

---
+++ Sets/Exercise_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using StreamReader sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(' ');
+                    Console.WriteLine(line);
+                    string name = line[0];
+                    DateTime instant = DateTime.Parse(line[1]);
+                    set.Add(new LogRecord{ Username = name, Instant = instant });
+                }
+                System.Console.WriteLine("Total users: " + set.Count);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+        }
+    }
+}
